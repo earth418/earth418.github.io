5acd20c961b29db9a3f3f4aa0f0849598702b758
Zach Malter <47525124+zmalter99@users.noreply.github.com>
2020-07-15 02:08:11 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+//smooth scroll
+const navBarHeight = document.querySelector("#navBarContainer").offsetHeight;
+document.querySelectorAll(`a[href^="#"]`).forEach(function (element) {
+    element.addEventListener("click", function (e) {
+        e.preventDefault();
+        window.scroll({
+            top: document.querySelector(this.getAttribute("href")).offsetTop - navBarHeight,
+            behavior: 'smooth'
+        });
+    });
+});
